17a855cb4248effdaba8be57f6c87293e48cff4b
p-robot <will@probert.co.nz>
2020-09-02 23:15:58 +0100

    Add call of Rscript for generating histogram of app users

--- step2_create_figures.sh
+++ step2_create_figures.sh
@@ -3,6 +3,8 @@
 # Create figures for the model paper
 
 
+source config.sh
+
 # Figures in the main text
 # ------------------------
 
@@ -19,4 +21,5 @@ python3 python/figure_6.py
 
 python3 python/figure_S1.py
 python3 python/figure_S2.py
+Rscript R/histogram_app_uptake.R $app_uptake
 
