9d242e406c196833816635462c214a6320c6e6e5
p-robot <will@probert.co.nz>
2020-09-02 13:16:14 +0100

    Update step2 shell script with shebang and comments

--- step2_create_figures.sh
+++ step2_create_figures.sh
@@ -1,6 +1,22 @@
+#!/usr/bin/env bash
+# 
+# Create figures for the model paper
 
-# Create figures
+
+# Figures in the main text
+# ------------------------
+
+# Figure 1 is the schematic of networks
 python3 python/figure_2.py
 python3 python/figure_3.py
+python3 python/figure_4.py
+# Figure 5 is the schematic of the model
+python3 python/figure_6.py
 
+
+# Supplementary figures
+# ---------------------
+
+python3 python/figure_S1.py
+python3 python/figure_S2.py
 
