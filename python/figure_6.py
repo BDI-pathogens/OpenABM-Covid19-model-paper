7c8d5a2df105c425bd207accc4d6d41298b435dd
p-robot <will@probert.co.nz>
2020-11-16 23:27:41 +0000

    Adjust figure_6.py to be callable

--- python/figure_6.py
+++ python/figure_6.py
@@ -8,7 +8,7 @@ Overall IFR, stratified by age
 
 from os.path import join
 
-import pandas as pd, numpy as np
+import pandas as pd, numpy as np, sys
 from matplotlib import pyplot as plt
 
 import plotting
@@ -19,14 +19,17 @@ age_group_labels = [enum.name[1:].replace("_","-") for enum in AgeGroupEnum]
 age_group_labels[-1] = "80+"
 
 if __name__ == "__main__":
-
+    
+    transmission_file = sys.argv[1]
+    output_figure = sys.argv[2]
+    
     plt.rcParams['figure.figsize'] = [12, 8]
     
-    df_trans = pd.read_csv(join("results", "transmission_Run1.csv"))
+    df_trans = pd.read_csv(transmission_file)
     
-    fig, ax = plotting.PlotHistIFRByAge(df_trans, "time_death", "time_infected", NBINS = n_age - 1, 
+    fig, ax = plotting.ifr_hist_by_age(df_trans, "time_death", "time_infected", NBINS = n_age - 1, 
         xticklabels = age_group_labels, xlabel = "Age group", age_group_var = "age_group_recipient")
     
-    plt.savefig(join("figures", "fig6_ifr"))
+    plt.savefig(output_figure)
     plt.close()
 
