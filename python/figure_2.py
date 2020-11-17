d02ed969e433e76624fb062c3a870f875e19f5ee
p-robot <will@probert.co.nz>
2020-11-17 20:04:07 +0000

    Adjust subpanels of figure2 so that bar charts of numbers of interactions are reported as fraction of the population; adjust to have input args

--- python/figure_2.py
+++ python/figure_2.py
@@ -5,22 +5,30 @@ Script to create subfigures for figure 2
 
 from os.path import join
 
-import pandas as pd, numpy as np
+import pandas as pd, numpy as np, sys
 from matplotlib import pyplot as plt
 
 import plotting, constants
 from COVID19.model import TransmissionTypeEnum
 
-# Import the data output from the model
-df_interact = pd.read_csv(join("results", "interactions_Run1.csv"))
-df_indiv = pd.read_csv(join("results", "individual_file_Run1.csv"))
-
-NBINS = 31
-bins = np.arange(NBINS)
+NBINS = 30
+bin_edges = 0.5 + np.arange(NBINS + 1)
 
 plt.rcParams["savefig.format"] = "png"
 
 if __name__ == "__main__":
+    
+    interaction_file = sys.argv[1]
+    individual_file = sys.argv[2]
+    output_dir = sys.argv[3]
+    
+    # Import the data output from the model
+    df_interact = pd.read_csv(interaction_file)
+    df_indiv = pd.read_csv(individual_file)
+    
+    # Find population size
+    n_total = df_indiv.shape[0]
+    
     plt.rcParams['figure.figsize'] = [6, 4]
     
     #############
@@ -37,16 +45,17 @@ if __name__ == "__main__":
         groupvar = "type", 
         binvar = "counts", 
         groups = constants.interaction_types, 
-        bins = bins, 
+        bins = bin_edges, 
         group_colours = plotting.network_colours, 
         group_labels = constants.interaction_labels, 
         xlimits = [-0.5, 30], 
         xlabel = "", 
         title = "", 
         legend_title = "Network", 
-        xticklabels = [])
+        xticklabels = [],
+        normalising_constant = n_total)
     
-    plt.savefig(join("figures", "fig2a_daily_interactions_by_network"))
+    plt.savefig(join(output_dir, "fig2a_daily_interactions_by_network.png"))
     plt.close()
     
     
@@ -65,17 +74,25 @@ if __name__ == "__main__":
         
         # Split by age group
         hist = df_agg.loc[df_agg["age_group_1"] == i].counts
-    
-        ax[i].hist(hist, bins, label = constants.age_group_labels[i], 
-            width = 0.8, color = "#0072B2", edgecolor = "#0072B2", 
-            linewidth = 0.5, zorder = 3)
+        
+        heights, bin_edges = np.histogram(hist, bin_edges)
+        
+        ax[i].bar(
+            x = bin_edges[1:] - 0.5, 
+            height = 100*heights/n_total, 
+            label = constants.age_group_labels[i], 
+            color = "#0072B2", 
+            edgecolor = "#0072B2", 
+            linewidth = 0.5, 
+            zorder = 3, 
+            width = 0.8)
         
         ax[i].spines["top"].set_visible(False)
         ax[i].spines["right"].set_visible(False)
         ax[i].set_ylabel("")
         ax[i].set_xlim([-0.5, 30])
-        ax[i].set_ylim([0, 15500])
-        ax[i].set_yticks([0, 15000])
+        ax[i].set_ylim([0, 1.5])
+        ax[i].set_yticks([0, 1.5])
         
         if i == (constants.n_age_groups - 1):
             ax[i].set_xlabel("", fontsize = 14)
@@ -89,7 +106,7 @@ if __name__ == "__main__":
             ha = 'left', va = 'bottom', 
             transform = ax[i].transAxes, fontsize = 16)
     
-    plt.savefig(join("figures", "fig2b_daily_interactions_by_age"))
+    plt.savefig(join(output_dir, "fig2b_daily_interactions_by_age.png"))
     plt.close()
     
     #####################
@@ -122,5 +139,5 @@ if __name__ == "__main__":
             yticklabels = constants.age_group_labels, 
             vmax = vmaxes[i])
         
-        plt.savefig(join("figures", "fig2{}_transmission_matrix_{}".format(index, network)))
+        plt.savefig(join(output_dir, "fig2{}_transmission_matrix_{}.png".format(index, network)))
         plt.close()
