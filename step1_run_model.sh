288fe28218e87e943fc1234b8b33396ee807aea7
p-robot <will@probert.co.nz>
2020-12-03 18:11:21 +0000

    Update step1 to use updated parameters

--- step1_run_model.sh
+++ step1_run_model.sh
@@ -10,16 +10,16 @@ source config.sh
 source venv/bin/activate
 
 # Run python script of an outbreak
-python3 python/covid_outbreak.py \
+python3 src/covid_outbreak.py \
     --input_parameter_file "$input_parameter_file" \
     --household_demographics_file "$household_demographics_file" \
     --output_dir "$output_dir" \
-    --app_uptake_multiplier $app_uptake_multiplier \
-    --lockdown_multiplier $lockdown_multiplier \
     --lockdown_duration $lockdown_duration \
     --lockdown_prevalence_trigger $lockdown_prevalence_trigger \
+    --intervention_prevalence_trigger $intervention_prevalence_trigger \
     --intervention_self_quarantine_fraction $intervention_self_quarantine_fraction \
-    --rng_seed $rng_seed --n_total $n_total
+    --rng_seed $rng_seed \
+    --n_total $n_total
 
 # Deactivate the Python virtual environment
 deactivate
